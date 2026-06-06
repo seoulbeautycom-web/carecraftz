bff4980694b2d52ebe231615aa21bea7453942a9
CareCraftz <seoulbeauty.com@gmail.com>
2026-06-06 04:39:18 +0400

    Add autoPlay and loop to video on About Us page

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -52,6 +52,8 @@ export default function AboutUs() {
           className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-full min-h-full object-cover"
           muted
           playsInline
+          autoPlay
+          loop
         />
       </div>
 
